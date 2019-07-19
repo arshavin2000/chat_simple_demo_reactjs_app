007174e1362b4606a2f62432693436593e667714
Achref Meghirbi <medachref.meghirbi@esprit.tn>
2019-07-19 18:31:52 +0100

    add animation

--- src/Message.js
+++ src/Message.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import './Message.css'
+import {rubberBand} from 'react-animations'
+import styled , {keyframes} from 'styled-components'
+
 
  function Message( {message="...", messages=["..."], username1="" , username2=""}) {
 
-   
+    const Bounce = styled.div`animation : 2s ${keyframes `${rubberBand}`} infinite`; 
+
 
     console.log("messages = " + messages);
 
     
     
     return(
+
     <ul className="Messages-list">
         {messages.map((m,i) => {
     const messageFromMe = i%2 !== 0;
     const className = messageFromMe ? "Messages-message currentMember" : "Messages-message";
     const backgorundPic = messageFromMe ? 'red' : 'cornflowerblue';
     const username = messageFromMe ? username2 : username1;
+
 
     return (
      <li className={className}>
@@ -36,15 +42,16 @@ import './Message.css'
     {message !== "" ? 
     <div className="Message-content">
       <div className="username">
-          Someone is writing
+         someone is typing...
       </div>
       <div className="text">{message}</div>
     </div> : 
     <div className="Message-content">
       <div className="username">
-          Someone is writing
+          someone is typing...
       </div>
-      <div className="text">...</div>
+      
+      <div className="text"><Bounce>...</Bounce></div>
     </div> }
       </ul>);
     
